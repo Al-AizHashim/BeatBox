6ae1cc937e574dea065407ee4b466408d5636e8d
Al-AizHashim <65592342+Al-AizHashim@users.noreply.github.com>
2020-12-03 08:11:11 +0300

    pause audio, when a call is incoming

--- app/src/main/java/alaiz/hashim/beatbox/MainActivity.kt
+++ app/src/main/java/alaiz/hashim/beatbox/MainActivity.kt
@@ -2,22 +2,65 @@ package alaiz.hashim.beatbox
 
 import alaiz.hashim.beatbox.databinding.ActivityMainBinding
 import alaiz.hashim.beatbox.databinding.ListItemSoundBinding
-
-import android.media.MediaPlayer
+import android.media.AudioAttributes
+import android.media.AudioFocusRequest
+import android.media.AudioManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.ViewGroup
 import android.widget.SeekBar
-import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AudioManager.OnAudioFocusChangeListener {
+    companion object{
+        lateinit var obj:MainActivity
+    }
+
+    var audioManager: AudioManager? = null
+    var audioFocusRequest: AudioFocusRequest?=null
+    @RequiresApi(Build.VERSION_CODES.O)
+     fun setAudioFocusChangeListener() {
+
+        audioManager = getSystemService(AUDIO_SERVICE) as AudioManager?
+
+
+        audioFocusRequest= AudioFocusRequest.Builder(AudioManager.AUDIOFOCUS_GAIN)
+            .setAudioAttributes(
+                AudioAttributes.Builder()
+                    .setUsage(AudioAttributes.USAGE_MEDIA)
+                    .setContentType(AudioAttributes.CONTENT_TYPE_MUSIC)
+                    .build()
+            )
+            .setAcceptsDelayedFocusGain(true)
+            .setOnAudioFocusChangeListener(this).build()
+        audioManager?.requestAudioFocus(audioFocusRequest!!)
+
+    }
+    override fun onAudioFocusChange(focusChange: Int) {
+        if(focusChange<=0){
+            beatBox.pause()
+        }else{
+            beatBox.resume()
+        }
+        
+    }
+
+
+
+
+
+
     private lateinit var beatBox: BeatBox
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        obj=MainActivity()
+        setAudioFocusChangeListener()
         beatBox = BeatBox(assets)
 
 
@@ -28,24 +71,25 @@ class MainActivity : AppCompatActivity() {
             adapter = SoundAdapter(beatBox.sounds)
         }
 
-        binding.seekBar.setOnSeekBarChangeListener(object :
-            SeekBar.OnSeekBarChangeListener {
-            override fun onProgressChanged(seek: SeekBar, progress: Int, fromUser: Boolean) {
+            binding.seekBar.setOnSeekBarChangeListener(object :
+                SeekBar.OnSeekBarChangeListener {
+                override fun onProgressChanged(seek: SeekBar, progress: Int, fromUser: Boolean) {
 
-                if (fromUser) {
-                    BeatBox.settingRate(progress.toFloat())
-                    binding.playBackSpeed.setText("Playback speed"+progress+"%")
+                    if (fromUser) {
+                        BeatBox.settingRate(progress.toFloat())
+                        binding.playBackSpeed.setText("Playback speed"+progress+"%")
+
+
+                    }
 
                 }
 
-            }
+                override fun onStartTrackingTouch(seek: SeekBar) {
 
-            override fun onStartTrackingTouch(seek: SeekBar) {
+                }
 
-            }
+                override fun onStopTrackingTouch(seek: SeekBar) {
 
-            override fun onStopTrackingTouch(seek: SeekBar) {
-                //Toast.makeText(this@MainActivity,"PlayBack Speed: " + seek.progress + "%",Toast.LENGTH_SHORT).show()
             }
         })
 
@@ -84,10 +128,13 @@ class MainActivity : AppCompatActivity() {
         }
 
         override fun onBindViewHolder(holder: SoundHolder, position: Int) {
+
             val sound = sounds[position]
             holder.bind(sound)
         }
 
         override fun getItemCount() = sounds.size
     }
+
+
 }
